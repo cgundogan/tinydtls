8cc8feaf10d9b1fe493d697b6d270f74e019ffa7
Raul Fuentes <ra.fuentes.sam@gmail.com>
2017-12-01 12:12:17 +0100

    crypto.c: Support for RIOT O.S. (and its static memory allocator)

--- crypto.c
+++ crypto.c
@@ -6,7 +6,7 @@
  * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
  *
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
@@ -35,54 +35,81 @@
 #include "prng.h"
 #include "netq.h"
 
-#ifndef WITH_CONTIKI
-#include <pthread.h>
-#endif
+#ifdef RIOT_VERSION
+#include <mutex.h>
+#endif /* RIOT_VERSION */
+
+#ifdef WITH_POSIX
+# include <pthread.h>
+#endif /* WITH_POSIX */
 
 #define HMAC_UPDATE_SEED(Context,Seed,Length)		\
   if (Seed) dtls_hmac_update(Context, (Seed), (Length))
 
 static struct dtls_cipher_context_t cipher_context;
-#ifndef WITH_CONTIKI
+
+#ifdef RIOT_VERSION
+static mutex_t cipher_context_mutex = MUTEX_INIT;
+#endif /* RIOT_VERSION */
+
+#ifdef WITH_POSIX
 static pthread_mutex_t cipher_context_mutex = PTHREAD_MUTEX_INITIALIZER;
-#endif
+#endif /* WITH_POSIX */
 
 static struct dtls_cipher_context_t *dtls_cipher_context_get(void)
 {
-#ifndef WITH_CONTIKI
-  pthread_mutex_lock(&cipher_context_mutex);
+
+#ifdef RIOT_VERSION
+    mutex_lock(&cipher_context_mutex);
 #endif
+
+#ifdef WITH_POSIX
+  pthread_mutex_lock(&cipher_context_mutex);
+#endif /* WITH_POSIX */
   return &cipher_context;
 }
 
 static void dtls_cipher_context_release(void)
 {
-#ifndef WITH_CONTIKI
+#ifdef RIOT_VERSION
+    mutex_unlock(&cipher_context_mutex);
+#endif /* RIOT_VERSION */
+
+#ifdef WITH_POSIX
   pthread_mutex_unlock(&cipher_context_mutex);
 #endif
 }
 
-#ifndef WITH_CONTIKI
-void crypto_init(void)
-{
+#ifdef RIOT_VERSION
+
+#include "memarray.h"
+MEMARRAY(handshake_storage, sizeof(dtls_handshake_parameters_t), DTLS_HANDSHAKE_MAX)
+MEMARRAY(security_storage,  sizeof(dtls_security_parameters_t), DTLS_SECURITY_MAX)
+
+void crypto_init(void) {
+  memarray_init(&handshake_storage, sizeof(dtls_handshake_parameters_t), DTLS_HANDSHAKE_MAX);
+  memarray_init(&security_storage, sizeof(dtls_security_parameters_t), DTLS_SECURITY_MAX);
 }
 
 static dtls_handshake_parameters_t *dtls_handshake_malloc(void) {
-  return malloc(sizeof(dtls_handshake_parameters_t));
-}
-
-static void dtls_handshake_dealloc(dtls_handshake_parameters_t *handshake) {
-  free(handshake);
-}
-
-static dtls_security_parameters_t *dtls_security_malloc(void) {
-  return malloc(sizeof(dtls_security_parameters_t));
+  return memarray_alloc(&handshake_storage);
 }
 
 static void dtls_security_dealloc(dtls_security_parameters_t *security) {
-  free(security);
+  memarray_free(&security_storage, security);
 }
-#else /* WITH_CONTIKI */
+
+static dtls_security_parameters_t *dtls_security_malloc(void) {
+  return memarray_alloc(&security_storage);
+}
+
+static void dtls_handshake_dealloc(dtls_handshake_parameters_t *handshake) {
+  memarray_free(&handshake_storage, handshake);
+}
+
+#endif /* RIOT_VERSION */
+
+#ifdef WITH_CONTIKI /* WITH_CONTIKI */
 
 #include "memb.h"
 MEMB(handshake_storage, dtls_handshake_parameters_t, DTLS_HANDSHAKE_MAX);
@@ -108,7 +135,30 @@ static dtls_security_parameters_t *dtls_security_malloc(void) {
 static void dtls_security_dealloc(dtls_security_parameters_t *security) {
   memb_free(&security_storage, security);
 }
+
 #endif /* WITH_CONTIKI */
+
+#ifdef WITH_POSIX
+void crypto_init(void)
+{
+}
+
+static dtls_handshake_parameters_t *dtls_handshake_malloc(void) {
+  return malloc(sizeof(dtls_handshake_parameters_t));
+}
+
+static void dtls_handshake_dealloc(dtls_handshake_parameters_t *handshake) {
+  free(handshake);
+}
+
+static dtls_security_parameters_t *dtls_security_malloc(void) {
+  return malloc(sizeof(dtls_security_parameters_t));
+}
+
+static void dtls_security_dealloc(dtls_security_parameters_t *security) {
+  free(security);
+}
+#endif /* WITH_POSIX */
 
 dtls_handshake_parameters_t *dtls_handshake_new(void)
 {
@@ -126,7 +176,7 @@ dtls_handshake_parameters_t *dtls_handshake_new(void)
     /* initialize the handshake hash wrt. the hard-coded DTLS version */
     dtls_debug("DTLSv12: initialize HASH_SHA256\n");
     /* TLS 1.2:  PRF(secret, label, seed) = P_<hash>(secret, label + seed) */
-    /* FIXME: we use the default SHA256 here, might need to support other 
+    /* FIXME: we use the default SHA256 here, might need to support other
               hash functions as well */
     dtls_hash_init(&handshake->hs_state.hs_hash);
   }
@@ -176,6 +226,7 @@ dtls_p_hash(dtls_hashfunc_t h,
 	    const unsigned char *random1, size_t random1len,
 	    const unsigned char *random2, size_t random2len,
 	    unsigned char *buf, size_t buflen) {
+  (void) h;
   dtls_hmac_context_t *hmac_a, *hmac_p;
 
   unsigned char A[DTLS_HMAC_DIGEST_SIZE];
@@ -221,11 +272,11 @@ dtls_p_hash(dtls_hashfunc_t h,
 
   dtls_hmac_init(hmac_p, key, keylen);
   dtls_hmac_update(hmac_p, A, dlen);
-  
+
   HMAC_UPDATE_SEED(hmac_p, label, labellen);
   HMAC_UPDATE_SEED(hmac_p, random1, random1len);
   HMAC_UPDATE_SEED(hmac_p, random2, random2len);
-  
+
   dtls_hmac_finalize(hmac_p, tmp);
   memcpy(buf, tmp, buflen - len);
 
@@ -236,7 +287,7 @@ dtls_p_hash(dtls_hashfunc_t h,
   return buflen;
 }
 
-size_t 
+size_t
 dtls_prf(const unsigned char *key, size_t keylen,
 	 const unsigned char *label, size_t labellen,
 	 const unsigned char *random1, size_t random1len,
@@ -245,16 +296,16 @@ dtls_prf(const unsigned char *key, size_t keylen,
 
   /* Clear the result buffer */
   memset(buf, 0, buflen);
-  return dtls_p_hash(HASH_SHA256, 
-		     key, keylen, 
-		     label, labellen, 
+  return dtls_p_hash(HASH_SHA256,
+		     key, keylen,
+		     label, labellen,
 		     random1, random1len,
 		     random2, random2len,
 		     buf, buflen);
 }
 
 void
-dtls_mac(dtls_hmac_context_t *hmac_ctx, 
+dtls_mac(dtls_hmac_context_t *hmac_ctx,
 	 const unsigned char *record,
 	 const unsigned char *packet, size_t length,
 	 unsigned char *buf) {
@@ -266,24 +317,25 @@ dtls_mac(dtls_hmac_context_t *hmac_ctx,
   dtls_hmac_update(hmac_ctx, record, sizeof(uint8) + sizeof(uint16));
   dtls_hmac_update(hmac_ctx, L, sizeof(uint16));
   dtls_hmac_update(hmac_ctx, packet, length);
-  
+
   dtls_hmac_finalize(hmac_ctx, buf);
 }
 
 static size_t
 dtls_ccm_encrypt(aes128_ccm_t *ccm_ctx, const unsigned char *src, size_t srclen,
-		 unsigned char *buf, 
+		 unsigned char *buf,
 		 unsigned char *nounce,
 		 const unsigned char *aad, size_t la) {
+
   long int len;
   (void)src;
 
   assert(ccm_ctx);
 
-  len = dtls_ccm_encrypt_message(&ccm_ctx->ctx, 8 /* M */, 
+  len = dtls_ccm_encrypt_message(&ccm_ctx->ctx, 8 /* M */,
 				 max(2, 15 - DTLS_CCM_NONCE_SIZE),
 				 nounce,
-				 buf, srclen, 
+				 buf, srclen,
 				 aad, la);
   return len;
 }
@@ -298,10 +350,10 @@ dtls_ccm_decrypt(aes128_ccm_t *ccm_ctx, const unsigned char *src,
 
   assert(ccm_ctx);
 
-  len = dtls_ccm_decrypt_message(&ccm_ctx->ctx, 8 /* M */, 
+  len = dtls_ccm_decrypt_message(&ccm_ctx->ctx, 8 /* M */,
 				 max(2, 15 - DTLS_CCM_NONCE_SIZE),
 				 nounce,
-				 buf, srclen, 
+				 buf, srclen,
 				 aad, la);
   return len;
 }
@@ -324,7 +376,7 @@ dtls_psk_pre_master_secret(unsigned char *key, size_t keylen,
 
   memcpy(p, result, sizeof(uint16));
   p += sizeof(uint16);
-  
+
   memcpy(p, key, keylen);
 
   return 2 * (sizeof(uint16) + keylen);
@@ -356,7 +408,7 @@ int dtls_ec_key_from_uint32_asn1(const uint32_t *key, size_t key_size,
 				 unsigned char *buf) {
   int i;
   unsigned char *buf_orig = buf;
-  int first = 1; 
+  int first = 1;
 
   for (i = (key_size / sizeof(uint32_t)) - 1; i >= 0 ; i--) {
     if (key[i] == 0)
@@ -366,7 +418,7 @@ int dtls_ec_key_from_uint32_asn1(const uint32_t *key, size_t key_size,
       *buf = 0;
       buf++;
       dtls_int_to_uint32(buf, key[i]);
-      buf += 4;      
+      buf += 4;
     } else if (first && !(key[i] & 0xFF800000)) {
       buf[0] = (key[i] >> 16) & 0xff;
       buf[1] = (key[i] >> 8) & 0xff;
@@ -443,7 +495,7 @@ dtls_ecdsa_create_sig_hash(const unsigned char *priv_key, size_t key_size,
   uint32_t priv[8];
   uint32_t hash[8];
   uint32_t rand[8];
-  
+
   dtls_ec_key_to_uint32(priv_key, key_size, priv);
   dtls_ec_key_to_uint32(sign_hash, sign_hash_size, hash);
   do {
@@ -466,7 +518,7 @@ dtls_ecdsa_create_sig(const unsigned char *priv_key, size_t key_size,
   dtls_hash_update(&data, server_random, server_random_size);
   dtls_hash_update(&data, keyx_params, keyx_params_size);
   dtls_hash_finalize(sha256hash, &data);
-  
+
   dtls_ecdsa_create_sig_hash(priv_key, key_size, sha256hash,
 			     sizeof(sha256hash), point_r, point_s);
 }
@@ -501,7 +553,7 @@ dtls_ecdsa_verify_sig(const unsigned char *pub_key_x,
 		      unsigned char *result_r, unsigned char *result_s) {
   dtls_hash_ctx data;
   unsigned char sha256hash[DTLS_HMAC_DIGEST_SIZE];
-  
+
   dtls_hash_init(&data);
   dtls_hash_update(&data, client_random, client_random_size);
   dtls_hash_update(&data, server_random, server_random_size);
@@ -513,7 +565,7 @@ dtls_ecdsa_verify_sig(const unsigned char *pub_key_x,
 }
 #endif /* DTLS_ECC */
 
-int 
+int
 dtls_encrypt(const unsigned char *src, size_t length,
 	     unsigned char *buf,
 	     unsigned char *nounce,
@@ -539,7 +591,7 @@ error:
   return ret;
 }
 
-int 
+int
 dtls_decrypt(const unsigned char *src, size_t length,
 	     unsigned char *buf,
 	     unsigned char *nounce,
@@ -564,4 +616,3 @@ error:
   dtls_cipher_context_release();
   return ret;
 }
-
